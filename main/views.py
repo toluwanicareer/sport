5ee6794f85dd3ece3b7697713444142f46942c12
Toluwani <abiodun.toluwanii@gmail.com>
2018-09-19 21:55:55 -0700

    'fgfg'

--- main/views.py
+++ main/views.py
@@ -72,9 +72,10 @@ class getActiveQuery(View):
         cat_id=request.GET.get('cat_id')
         #active_queries=Query.c_objects.active(cat_id)
         now=timezone.now().date()
-        active_trends=Track.objects.filter(date=now).filter(query__category__id=cat_id).order_by('-query__query_date')
+        active_trends=Track.objects.filter(date=now).filter(query__category__id=cat_id)#.order_by('-query__query_date')
         categories=Category.objects.all()
         sport=Category.objects.get(id=cat_id)
+        #pdb.set_trace()
         #response=render_to_string('includes/query_list.html',{'queries':active_trends})
         return render(request, 'queries.html', {'trends':active_trends, 'category':categories, 'sport':sport,  'cat_tile':'Active ' + sport.name})
 
